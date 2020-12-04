ec32b2998fda7aeac39e70e24c4c84bd41012e26
Jonas Törnmalm <jonas.tornmalm@hotmail.com>
2020-12-04 11:36:46 +0100

    Add project files.

---
+++ TollFeeCalculatorTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TollFeeCalculator;
+
+namespace TollFeeCalculatorTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
